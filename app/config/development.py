ef4c72f3987a2190d972611e1683bd03e6170ad3
wly314 <wangliuyang@360youmei.com>
2017-04-21 18:09:58 +0800

    提交base.html

--- app/config/development.py
+++ app/config/development.py
@@ -7,3 +7,4 @@ DEBUG = True
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
         'postgresql://postgres:pgsql@127.0.0.1:5432/teamblog'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
